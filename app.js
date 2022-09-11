e7580548371aacbc8f683cd5f1ab0f41bbb74dd1
phuongvu0804 <99994868+phuongvu0804@users.noreply.github.com>
2022-09-11 10:13:36 +0300

    Feat: add comma

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const apiKeyWeather = "770de780ae88163d36266e3e2b0835f2";
 const apiKeyLocation = "T5LHuF1z6+qQ3rY3oJ993w==NG6SjZHK9qkF26q6";
 
-const locationsArray = [ "Hanoi", "Espoo", "Berlin"]
-
 const currentLocation = document.querySelector(".content__location");
 const currentWeather = document.querySelector(".main-info__content h2");
 const currentTemp = document.querySelector(".main-info__content p");
@@ -13,47 +11,46 @@ const locationSelection = document.querySelector(".location__input");
 const errorMsg = document.querySelector(".app__msg--error");
 const mainContent = document.querySelector(".content__main-info");
 
-let currentTempData = {}
+let currentTempData = {};
 let isCelcius = true;
 let darkTheme = true;
 
 const convertTempToCel = (temp) => {
-    return Math.round(temp - 273.15)
-} 
+    return Math.round(temp - 273.15);
+};
 
 const convertTempToFah = (temp) => {
-    return Math.round((temp - 273.15) * 9/5 + 32)
-} 
+    return Math.round((temp - 273.15) * 9/5 + 32);
+}; 
 
 const convertToKmPerHour = (speed) => {
-    return Math.round(speed * 3.6)
-}
+    return Math.round(speed * 3.6);
+};
 
 const handleDisplayData = (data) => {
     currentLocation.innerText = data.name;
     currentTemp.innerText = `${convertTempToCel(data.main.temp)}°C`;
     currentWeather.innerText = data.weather[0].main
 
-    windData.innerText = `${convertToKmPerHour(data.wind.speed)} km/h`
-    cloudData.innerText = `${data.clouds.all}%`
-    feelLikeData.innerText = `${convertTempToCel(data.main.feels_like)}°C`
+    windData.innerText = `${convertToKmPerHour(data.wind.speed)} km/h`;
+    cloudData.innerText = `${data.clouds.all}%`;
+    feelLikeData.innerText = `${convertTempToCel(data.main.feels_like)}°C`;
 }
 
 const handleDisplayError = (error) => {
-    console.log(error)
     errorMsg.classList.add("d-block");
     errorMsg.innerText = error;
-}
+};
 
 const handleStyleButton = (buttonClass) => {
     const buttons = document.querySelectorAll(".app-foot__btn");
     buttons.forEach((button) => {
         if (button.className.includes("active")) {
             button.classList.remove("active")
-        }
-    })
-    document.querySelector(buttonClass).classList.add("active")
-}
+        };
+    });
+    document.querySelector(buttonClass).classList.add("active");
+};
 
 const fetchWeatherData = (lat, lon) => {
     const weatherApi = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKeyWeather}`
@@ -72,7 +69,7 @@ const fetchWeatherData = (lat, lon) => {
             handleDisplayData(data);
         })
         .catch((error) => {
-            handleDisplayError(error)
+            handleDisplayError(error);
         });
 }
 
@@ -87,20 +84,20 @@ const getCurrentWeatherData = () => {
             lat = position.coords.latitude;
             
             fetchWeatherData(lat, lon)
-        }, (error) => errorMsg.innerText = error)
-    }  
-}
+        }, (error) => errorMsg.innerText = error);
+    };  
+};
 
 window.addEventListener("load", () => {
-    getCurrentWeatherData()
-})
+    getCurrentWeatherData();
+});
 
 
 //Search location
 document.querySelector(".app-foot__btn.app-foot__btn--search").onclick = () => {
     handleStyleButton(".app-foot__btn.app-foot__btn--search");
     locationSelection.focus();
-}
+};
 
 const handleSearchLocation = () => {
     const searchInput = document.querySelector(".location__input").value;
@@ -114,12 +111,12 @@ const handleSearchLocation = () => {
         .then(response => response.json())
         .then(data => {
             if (data.length) {
-                const location = data[0]
-                fetchWeatherData(location.latitude, location.longitude)
+                const location = data[0];
+                fetchWeatherData(location.latitude, location.longitude);
             }
         })
-        .catch((error) => handleDisplayError(error))
-}
+        .catch((error) => handleDisplayError(error));
+};
 
 //Change from Celsius to Fahrenheit
 mainContent.onclick = () => { 
@@ -133,17 +130,16 @@ mainContent.onclick = () => {
         currentTemp.innerText = celTemp + "°C";
 
     }
-} 
+};
 
 //Back to home function
 document.querySelector(".app-foot__btn.app-foot__btn--home").onclick = () => {
-    handleStyleButton(".app-foot__btn.app-foot__btn--home")
+    handleStyleButton(".app-foot__btn.app-foot__btn--home");
     getCurrentWeatherData();
-}
+};
 
 //Change dark/light theme
 const handleStyleTheme = () => {
-    console.log(darkTheme)
     if (darkTheme) {
         document.querySelector(".theme__select-btn#dark").classList.add("active");
         document.querySelector(".theme__select-btn#light").classList.remove("active");
@@ -152,7 +148,7 @@ const handleStyleTheme = () => {
         document.querySelector(".theme__select-btn#light").classList.add("active");
     }
 
-}
+};
 
 handleStyleTheme();
 
@@ -161,12 +157,12 @@ const handleChangeTheme = (theme) => {
     if (theme) {
         darkTheme = true;
         handleStyleTheme();
-        document.querySelector("#weather-app").classList.remove("light")
+        document.querySelector("#weather-app").classList.remove("light");
 
     } else {
         darkTheme = false;
         handleStyleTheme();
-        document.querySelector("#weather-app").classList.add("light")
+        document.querySelector("#weather-app").classList.add("light");
 
-    }
-}
+    };
+};
